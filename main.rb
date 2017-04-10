6397b7aa2f525c0c91b21af3c2ab56bd597e9729
nidise <nidise@gmail.com>
2017-04-10 12:22:53 +0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -18,6 +18,10 @@ helpers do
     images.map { |image| image }.join
   end
 
+  def paint_images_sizes(images)
+    images.map { |width| image }.join
+  end
+  
   def links_to_li(links)
     links.map { |link| "<li><a href='#{link}'>#{link}</a></li>" }.join("\n")
   end
